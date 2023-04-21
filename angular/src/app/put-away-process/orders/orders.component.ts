51186816e6f8cb6be525db87e4ac8acc269a18cd
Jakub Baranowski <jakub@curvestone.io>
2023-04-21 12:27:09 +0200

    some changes

--- angular/src/app/put-away-process/orders/orders.component.ts
+++ angular/src/app/put-away-process/orders/orders.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { OrderDto } from '@proxy/put-away-process/application/contracts/orders';
+import { OrderService } from '@proxy/put-away-process/application/orders';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+  styleUrls: ['./orders.component.scss'],
+  providers: [ListService],
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  orders = { items: [], totalCount: 0 } as PagedResultDto<OrderDto>;
 
+  constructor(public readonly list: ListService,
+              private orderService: OrderService) { }
+
+  ngOnInit(): void {
+    const orderStreamCreator = query => this.orderService.getList(query);
+
+    this.list.hookToQuery(orderStreamCreator).subscribe(response => {
+      this.orders = response;
+    });
+  }
 }
--- angular/src/app/put-away-process/put-away-process.module.ts
+++ angular/src/app/put-away-process/put-away-process.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { PutAwayProcessRoutingModule } from './put-away-process-routing.module';
 import { PutAwayProcessComponent } from './put-away-process.component';
 import { OrdersComponent } from './orders/orders.component';
+import { SharedModule } from '../shared/shared.module';
+import { UiExtensionsModule } from '@abp/ng.theme.shared/extensions';
+import { PageModule } from '@abp/ng.components/page';
 
 
 @NgModule({
@@ -12,8 +15,8 @@ import { OrdersComponent } from './orders/orders.component';
     OrdersComponent
   ],
   imports: [
-    CommonModule,
-    PutAwayProcessRoutingModule
+    PutAwayProcessRoutingModule,
+    SharedModule,
   ]
 })
 export class PutAwayProcessModule { }
